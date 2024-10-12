5014d320daeccd355e0e09a3990d46e9707d8f75
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2024-10-12 18:34:49 -0300

    refactor(header): updates class names pattern

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -10,19 +10,19 @@ type HeaderType = {
 const Header = ({ children }: HeaderType)  => {
   return (
     <header className={styles.header}>
-      <Container>
-        <nav className={styles.header__navbar}>
-          <Image className={styles.header__navbar__menu} src="/icons/menu.svg" alt="menu" width="16" height="15"/>
-          <Image className={styles.header__navbar__logo} src="/images/audiophile.png" alt="logo" width="143" height="25" />
-          <ul className={styles.header__navbar__list}>
-            <li className={styles.header__navbar__list__item}>Home</li>
-            <li className={styles.header__navbar__list__item}>Headphones</li>
-            <li className={styles.header__navbar__list__item}>Speakers</li>
-            <li className={styles.header__navbar__list__item}>Earphones</li>
+      <Container fullwidth>
+        <nav className={styles.navbar}>
+          <Image className={styles.menuIcon} src="/icons/menu.svg" alt="menu" width="16" height="15"/>
+          <Image className={styles.logo} src="/images/audiophile.png" alt="logo" width="143" height="25" />
+          <ul className={styles.list}>
+            <li className={styles.listItem}>Home</li>
+            <li className={styles.listItem}>Headphones</li>
+            <li className={styles.listItem}>Speakers</li>
+            <li className={styles.listItem}>Earphones</li>
           </ul>
           <Image src="/icons/cart-shop.svg" alt="shopping-cart" width="24" height="20" />
         </nav>
-        { !!children && <div className={styles.header__navbar__divider}></div> }
+        { !!children && <div className={styles.divider}></div> }
         { children }
       </Container>
     </header>
