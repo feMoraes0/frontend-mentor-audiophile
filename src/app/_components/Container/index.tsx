bdd8e2d37a895012852582f71453c24a0231d09b
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2024-10-12 18:35:36 -0300

    feat(container): adds fluid style

--- src/app/_components/Container/index.tsx
+++ src/app/_components/Container/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import styles from './style.module.scss';
 
 type ContainerType = {
-  children: Readonly<React.ReactNode>
+  children: Readonly<React.ReactNode>,
+  fullwidth?: Readonly<boolean>
 }
 
-const Container = ({ children }: ContainerType) => {
+const Container = ({ children, fullwidth }: ContainerType) => {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${fullwidth ? styles.containerFluid : ''}`}>
       { children }
     </div>
   );
