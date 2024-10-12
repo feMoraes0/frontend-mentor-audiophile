b23365cc78fe483976b4369c590a1d95c7c1ac50
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2024-10-12 18:28:21 -0300

    feat(home): adds header background image and content

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -1,9 +1,19 @@
 import Header from "@components/Header";
+import styles from '@pages/home/page.module.scss';
 
 export default function HomePage() {
   return (
-    <main>
-      <Header />
+    <main className={styles.home}>
+      <Header>
+        <div className={styles.header}>
+          <h2 className={styles.headerSubtitle}>New Product</h2>
+          <h1 className={styles.headerTitle}>XX99 Mark II HeadphoneS</h1>
+          <p className={styles.headerContent}>
+            Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.
+          </p>
+          <button className={styles.headerButton}>See Product</button>
+        </div>
+      </Header>
     </main>
   );
 }
