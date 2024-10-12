b280a7ab9dce80fa70a4e308b5b009c6ceedb09e
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2024-10-11 21:38:59 -0300

    chore(route): updates initial route to home and removes base pages files

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./page.module.scss";
-
-export default function HomePage() {
-  return (
-    <div className={styles.main}>
-      <p className={styles.title_1}>Hello World</p>
-    </div>
-  );
-}
