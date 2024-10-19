7be2d7958d688d1443662115ac73ec0136b23122
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2024-10-19 20:16:38 -0300

    refactor(container): updates props with child node array support and styling

--- src/app/_components/Container/index.tsx
+++ src/app/_components/Container/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import styles from './style.module.scss';
 
 type ContainerType = {
-  children: Readonly<React.ReactNode>,
-  fullwidth: Readonly<boolean>;
+  children: Readonly<React.ReactNode | React.ReactNode[]>,
+  fullwidth?: Readonly<boolean>,
+  className?: Readonly<string>,
 }
 
-const Container = ({ children, fullwidth = false }: ContainerType) => (
-  <div className={`${styles.container} ${fullwidth ? styles.container_fluid : ''}`}>
+const Container = ({ children, fullwidth = false, className }: ContainerType) => (
+  <div className={`${styles.container} ${fullwidth ? styles.container_fluid : ''} ${className}`}>
     { children }
   </div>
 );
